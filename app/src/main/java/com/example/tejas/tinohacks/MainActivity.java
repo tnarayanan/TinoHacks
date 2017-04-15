5fb4cd56d26e9dd5fcea838f26793d1b28ebf655
tnarayanan <tejasn100@gmail.com>
2017-04-15 12:05:14 -0700

    Recycler View

--- app/src/main/java/com/example/tejas/tinohacks/MainActivity.java
+++ app/src/main/java/com/example/tejas/tinohacks/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.tejas.tinohacks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        rv = (RecyclerView) findViewById(R.id.rv);
     }
 }
