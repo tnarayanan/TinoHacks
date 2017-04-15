c2dbc89cff468eda4656be8ae8ce60a72d9206a0
Ashboy64 <ashish.arartc@gmail.com>
2017-04-15 15:17:40 -0700

    Login and dependencies changed

---
+++ app/src/main/java/com/example/tejas/tinohacks/detailedActivity.java
@@ -0,0 +1,13 @@
+package com.example.tejas.tinohacks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class detailedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed);
+    }
+}
