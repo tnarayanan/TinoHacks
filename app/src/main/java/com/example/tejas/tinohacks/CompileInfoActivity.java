280fcd890589dc3cc4256d12a026849af5183128
TheEccentricScientist <sbsripad@gmail.com>
2017-04-15 11:56:56 -0700

    HEEEEY PULL NOW

---
+++ app/src/main/java/com/example/tejas/tinohacks/CompileInfoActivity.java
@@ -0,0 +1,13 @@
+package com.example.tejas.tinohacks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompileInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compile_info);
+    }
+}
