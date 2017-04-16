9c1af749bfb722129ae41599688a26c658f008d8
Ashboy64 <ashish.arartc@gmail.com>
2017-04-15 23:40:29 -0700

    Things

--- app/src/main/java/com/example/tejas/tinohacks/detailedActivity.java
+++ app/src/main/java/com/example/tejas/tinohacks/detailedActivity.java
@@ -2,10 +2,19 @@ package com.example.tejas.tinohacks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class detailedActivity extends AppCompatActivity {
+
+    TextView title;
+    TextView article;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,24 @@ public class detailedActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         int policyID = extras.getInt("policyID");
 
+        title = (TextView) findViewById(R.id.title);
+        article = (TextView) findViewById(R.id.article);
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        database.getReference("policies").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot ChildDataSnapshot: dataSnapshot.getChildren()){
+                    title.setText(ChildDataSnapshot.child("title").getValue(String.class));
+                    article.setText(ChildDataSnapshot.child("article").getValue(String.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
