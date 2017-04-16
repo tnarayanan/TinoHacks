0c6503efe333849c761c829f6db5e80abf3ddbb4
pguduguntla <praneethg347@gmail.com>
2017-04-15 20:32:29 -0700

    hello. this is the protest organizat

--- app/src/main/java/com/example/tejas/tinohacks/ProtestOrganization.java
+++ app/src/main/java/com/example/tejas/tinohacks/ProtestOrganization.java
@@ -1,11 +1,82 @@
 package com.example.tejas.tinohacks;
 
+import android.icu.util.Calendar;
+import android.location.Address;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.Format;
+
 
 public class ProtestOrganization extends AppCompatActivity {
 
-    @Override
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    public static String policy = "hello";
+
+    public static String userAddress;
+    public static String protestTime;
+
+    public void submitButton(){
+        final DatePicker datePicker = (DatePicker) findViewById(R.id.datePicker1);
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth() + 1;
+        int year = datePicker.getYear();
+
+        final TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
+        int hour = timePicker.getHour();
+        int minute = timePicker.getMinute();
+
+        final EditText protestAddress = (EditText) findViewById(R.id.protestAddress);
+
+        userAddress = protestAddress.getText().toString();
+
+        String am_pm = "";
+
+        if(hour<11){
+            am_pm = "AM";
+        }else{
+            am_pm = "PM";
+            hour = 24-hour;
+        }
+
+        protestTime = hour + ":" + minute + " " + am_pm;
+
+
+        database.getReference("Protests").child(policy).child(userAddress).setValue(protestTime);
+
+
+        database.getReference("Policies").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot childDataSnapshot:dataSnapshot.getChildren()){
+                    database.getReference("Protests").child(childDataSnapshot.getKey().toString()).setValue(null);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+
+
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_protest_organization);
---
+++ app/src/main/java/com/example/tejas/tinohacks/ProtestsMap.java
@@ -0,0 +1,103 @@
+package com.example.tejas.tinohacks;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.location.Geocoder;
+import android.location.Address;
+import android.view.View;
+
+
+import java.io.IOException;
+import java.util.List;
+
+public class ProtestsMap extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_protests_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+
+
+    public LatLng getLocationFromAddress(Context context, String strAddress) {
+
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
+
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+        }
+
+        return p1;
+    }
+
+    private void setUpMap(){
+        LatLng pos = getLocationFromAddress(getApplicationContext(),ProtestOrganization.userAddress);
+        Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("This protest is organized for this policy: " + ProtestOrganization.policy).snippet("Time: " + ProtestOrganization.protestTime                                                                                                                                              ));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(pos));
+
+
+    }
+
+    public void backButtonClicked(View view){
+
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+    }
+
+}
